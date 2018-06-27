08d161db44b76eaa3fcc0e18bfe0d5ee4b02e44a
monisahmad <monis.ahmad42@gmail.com>
2018-06-27 22:56:07 +0530

    WIP:Monis Segment 1

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hey, I am server response');
 });
 
+app.get('/movie/:title', (req, res) => {
+  res.send(`My favorite movie is ${req.params.title}`);
+});
+
 app.listen(3000, () => console.log('Server listening on port: 3000s'));
 
 // 2) Start server on port 3000
