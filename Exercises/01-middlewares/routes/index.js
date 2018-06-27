1ec5d558ec5bb68f412639dbef7962a7131c5601
monisahmad <monis.ahmad42@gmail.com>
2018-06-28 05:06:34 +0530

    Monis_Navneet-Segment-2

---
+++ Exercises/01-middlewares/routes/index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Hello' });
+});
+
+module.exports = router;
