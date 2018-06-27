1e113c80c10ae43ee0dda1339234f202df50dc58
Amal <amaledassery@gmail.com>
2018-06-27 23:21:58 +0530

    first

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.send('Hello World'));
+app.listen(
+  3000,
+  () => console.log('Example'),
+);
 // 1) Create Routes
 
 // 2) Start server on port 3000
