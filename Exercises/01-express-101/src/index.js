1c87b02169c261ae5b9d33d26ba00b94ea6dc5f0
monisahmad <monis.ahmad42@gmail.com>
2018-06-27 22:49:36 +0530

    WIP:Monis Segment 1

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 
 // 1) Create Routes
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
+
+app.listen(3000, () => console.log('Server listening on port: 3000s'));
 
 // 2) Start server on port 3000
 
