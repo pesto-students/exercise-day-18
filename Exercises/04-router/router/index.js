d29f9324b8a99b485390a24cecd3549d630919ea
navneet <navneet.sahota@outlook.com>
2018-06-28 00:16:36 +0530

    Q4 in progress

--- Exercises/04-router/router/index.js
+++ Exercises/04-router/router/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const router = express.Router();
-const language = [];
+const languages = [];
 
 router.get('/', (req, res) => {
-  res.render('../views/index', () => {
-    res.send(language);
-  });
+  res.render('index', { languages });
 });
 
+router.get('/new', (req, res) => {
+  res.render('new');
+});
+
+router.post('/new', (req, res) => {
+  languages.push(req.body.name);
+  res.redirect('index');
+});
 module.exports = router;
