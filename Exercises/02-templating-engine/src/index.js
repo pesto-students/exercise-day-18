c67e069bb8ab561b890873153adcb6cf83e05dff
navneet <navneet.sahota@outlook.com>
2018-06-27 23:27:48 +0530

    Q2

--- Exercises/02-templating-engine/src/index.js
+++ Exercises/02-templating-engine/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
-app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
+app.get('/', (_, res) => {
+  res.render('../views/index', () => {
+    res.send(languages);
+  });
+  // Use res.render() to render the ejs file instead of sending text response
 });
 
 app.listen(3000, () => {
