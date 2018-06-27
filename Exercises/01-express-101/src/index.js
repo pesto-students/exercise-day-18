7907947347d320fdd34885fb64270f986555318e
Nikhil Arora <narora200@gmail.com>
2018-06-27 22:45:07 +0530

    WIP

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -4,6 +4,14 @@ const app = express();
 
 // 1) Create Routes
 
+app.get('/', (req, res) => {
+  res.send('Serving the "/" page');
+});
+
 // 2) Start server on port 3000
+
+app.listen(3000, () => {
+  console.log("Server started on port: 3000");
+});
 
 module.exports = app;
