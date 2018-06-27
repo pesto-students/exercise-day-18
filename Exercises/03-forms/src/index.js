fe5810bd1f5cff499cfbf9ee11a0f1effd4640e5
Kundan Singh <kundan.singh979@gmail.com>
2018-06-27 23:27:51 +0530

    WIP: Deprication warning fix for BodyParser

--- Exercises/03-forms/src/index.js
+++ Exercises/03-forms/src/index.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser());
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 app.set('view engine', 'ejs');
 
 /* HANDLE POST REQUESTS */
