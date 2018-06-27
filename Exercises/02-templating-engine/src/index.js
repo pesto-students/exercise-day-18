55e64f6fc9c24caa2d8f620141a15c216c33c2eb
Kundan Singh <kundan.singh979@gmail.com>
2018-06-27 23:14:45 +0530

    WIP: EJS solution

--- Exercises/02-templating-engine/src/index.js
+++ Exercises/02-templating-engine/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+// const ejs = require('ejs');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
 app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
+  // return res.send(`Hello ${languages[0]}`);
+  // Use res.render() to render the ejs file instead of sending text response
+  res.render('index', { languages });
 });
 
 app.listen(3000, () => {
