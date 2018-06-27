6af90731bf83db65289125bfbdd7449660f1b212
navneet <navneet.sahota@outlook.com>
2018-06-27 23:09:27 +0530

    Q1

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -3,7 +3,17 @@ const express = require('express');
 const app = express();
 
 // 1) Create Routes
+app.get('/', (_, res) => {
+  res.send('Hey, I am server response');
+});
 
+app.get('/movie/:title', (req, res) => {
+  res.send(`My favorite movie is ${req.params.title}`);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port: 3000');
+});
 // 2) Start server on port 3000
 
 module.exports = app;
