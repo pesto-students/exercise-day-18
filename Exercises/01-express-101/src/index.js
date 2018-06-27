40559c65192201010ed5db265c75ab1b2a812fd7
Amal <amaledassery@gmail.com>
2018-06-28 00:01:44 +0530

    WIP

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
-app.listen(
-  3000,
-  () => console.log('Example'),
-);
+app.get('/', (req, res) => {
+  res.send('Hello i am server ');
+});
+
+app.get('/movie/:title', (req, res) => {
+  res.send(`My Fav movie is ${req.params.title}`);
+});
+app.listen(PORT, () => {
+  console.log(`Example: ${PORT}`);
+});
 // 1) Create Routes
 
 // 2) Start server on port 3000
