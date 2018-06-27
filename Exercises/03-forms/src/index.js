80c8975f4a5b13e1911f1c6bc3f769bb91958ab1
monisahmad <monis.ahmad42@gmail.com>
2018-06-28 00:16:52 +0530

    WIP: Excercise 03

--- Exercises/03-forms/src/index.js
+++ Exercises/03-forms/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
 });
@@ -10,8 +12,21 @@ app.get('/form-with-get', (req, res) => {
   return res.render('form-with-get');
 });
 
+app.get('/submit-form-with-get', (req, res) => {
+  return res.render('submit-form-with-get', {
+    response: JSON.stringify({
+      first: req.query.first,
+      last: req.query.last,
+    }),
+  });
+});
+
 app.get('/form-with-post', (req, res) => {
   return res.render('form-with-post');
+});
+
+app.get('/submit-form-with-post', (req, res) => {
+  return res.render('submit-form-with-post');
 });
 
 app.listen(3000, () => {
