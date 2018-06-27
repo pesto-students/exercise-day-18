b7913f781c6b34322d17e5b9baa16662a69afa82
navneet <navneet.sahota@outlook.com>
2018-06-28 00:04:16 +0530

    Q3

--- Exercises/03-forms/src/index.js
+++ Exercises/03-forms/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
@@ -10,9 +14,18 @@ app.get('/form-with-get', (req, res) => {
   return res.render('form-with-get');
 });
 
+app.get('/submit-form-with-get', (req, res) => {
+  res.send(req.query);
+});
+
 app.get('/form-with-post', (req, res) => {
   return res.render('form-with-post');
 });
+
+app.post('/submit-form-with-post', (req, res) => {
+  res.send(req.body);
+});
+
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
--- Exercises/04-router/router/index.js
+++ Exercises/04-router/router/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
+const language = [];
+
+router.get('/', (req, res) => {
+  res.render('../views/index', () => {
+    res.send(language);
+  });
+});
 
 module.exports = router;
