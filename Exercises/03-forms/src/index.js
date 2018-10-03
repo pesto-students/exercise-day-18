4e2b2420059711bf53f76501182846e60e5c780f
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-04 00:27:36 +0530

    Finished ex 3

--- Exercises/03-forms/src/index.js
+++ Exercises/03-forms/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
@@ -12,6 +16,20 @@ app.get('/form-with-get', (req, res) => {
 
 app.get('/form-with-post', (req, res) => {
   return res.render('form-with-post');
+});
+
+app.get('/submit-form-with-get', (req, res) => {
+  res.end(JSON.stringify({
+    firstName: req.query.first,
+    lastName: req.query.last,
+  }));
+});
+
+app.post('/submit-form-with-post', (req, res) => {
+  res.end(JSON.stringify({
+    firstName: req.body.first,
+    lastName: req.body.last,
+  }));
 });
 
 app.listen(3000, () => {
