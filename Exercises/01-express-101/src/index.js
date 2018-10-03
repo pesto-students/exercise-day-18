b66b7915f23c9140214b3bfc04ae7852c84f917b
Bharani <bharanidharan.nb@gmail.com>
2018-10-04 00:05:14 +0530

    01-express-101

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -4,6 +4,20 @@ const app = express();
 
 // 1) Create Routes
 
+app.route('/')
+  .get((req, res) => {
+    res.send('Hey, I am server response');
+  });
+
+app.route('/movie/:title')
+  .get((req, res) => {
+    res.send(`My favorite movie is ${req.params.title}`);
+  });
+
 // 2) Start server on port 3000
+
+app.listen(3000, () => {
+  console.log('Server listening on port: 3000');
+});
 
 module.exports = app;
