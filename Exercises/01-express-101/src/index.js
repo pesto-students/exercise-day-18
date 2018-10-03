90c841ad3e1b38ad7998d3e8440b55be12bf21ca
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-10-04 00:04:45 +0530

    Finished ex 1

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
 const app = express();
+const config = {
+  PORT: process.env.PORT || 3000,
+};
 
 // 1) Create Routes
+app.get('/', (req, res) => {
+  res.end('Hey, I am server response');
+});
+
+app.get('/movie/:title', (req, res) => {
+  res.end(`My favorite movie is ${req.params.title}`);
+});
 
 // 2) Start server on port 3000
+app.listen(config.PORT, () => {
+  // eslint-disable-next-line
+  console.log(`Server listening on port: ${config.PORT}`);
+});
 
 module.exports = app;
