b4b03253fd0a93d5e0918ccb70849b5be03efab3
vipulrawat <vipulrawat.imi@live.in>
2018-10-04 03:44:31 +0530

    [WIP]

--- Exercises/01-express-101/src/index.js
+++ Exercises/01-express-101/src/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require('express'); // eslint-disable-line
 
 const app = express();
 
 // 1) Create Routes
-
+app.get('/', (req, res) => res.send('Hey, I am server response'));
+app.get('/movie/:id', (req, res) => res.send(`My favorite movie is ${req.params.id}`));
 // 2) Start server on port 3000
-
+app.listen(3000);
 module.exports = app;
