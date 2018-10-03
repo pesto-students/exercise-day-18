bdc1cfe6b736469e164e77476079db924d2ec246
vipulrawat <vipulrawat.imi@live.in>
2018-10-04 00:34:20 +0530

    [WIP] Ex 01 - 03

--- Exercises/03-forms/src/index.js
+++ Exercises/03-forms/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   return res.redirect('/form-with-get');
@@ -12,6 +17,22 @@ app.get('/form-with-get', (req, res) => {
 
 app.get('/form-with-post', (req, res) => {
   return res.render('form-with-post');
+});
+
+app.get('/submit-form-with-get', (req, res) => {
+  const response = {
+    first: req.query.first,
+    last: req.query.last,
+  };
+  res.send(JSON.stringify(response));
+});
+
+app.post('/submit-form-with-post', (req, res) => {
+  const response = {
+    first: req.body.first,
+    last: req.body.last,
+  };
+  res.send(JSON.stringify(response));
 });
 
 app.listen(3000, () => {
